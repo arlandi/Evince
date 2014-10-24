7f7ad7c592adcb37057b76641cbabb641d3f4ae7
Anggi Priatmadi <anggi.arlandi@gmail.com>
2014-10-24 16:15:18 -0400

    added cloudcode

---
+++ www/cloudcode/cloud/main.js
@@ -0,0 +1,30 @@
+Parse.Cloud.afterSave("Message", function(request) {
+
+  // Build fromUser
+  var fromUser = request.object.get('fromUser');
+
+  // Build toUser
+  var toUser = request.object.get('toUser');
+
+  // The message
+  var message = request.object.get('message');
+
+  // Build the query
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo('userID', toUser.id);
+
+  Parse.Push.send({
+    where: pushQuery, // Set our Installation query
+    data: {
+      alert: fromUser.get('username') + ": * " + message + " *",
+      sound: "default"
+    }
+  }, {
+    success: function() {
+      console.log('successful push');
+    },
+    error: function(error) {
+      throw "Got an error " + error.code + " : " + error.message;
+    }
+  });
+});
