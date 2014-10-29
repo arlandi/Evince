ac64608f7e6e23387c7e872f3bee0b17e4f1223c
Anggi Priatmadi <anggi.arlandi@gmail.com>
2014-10-29 07:36:42 -0400

    new friend push notification

--- www/cloudcode/cloud/main.js
+++ www/cloudcode/cloud/main.js
@@ -1,3 +1,4 @@
+// New Message Push Notification
 Parse.Cloud.afterSave("Message", function(request) {
 
   // Build fromUser
@@ -21,7 +22,36 @@ Parse.Cloud.afterSave("Message", function(request) {
     }
   }, {
     success: function() {
-      console.log('successful push');
+      console.log('Evince Push Notification Success');
+    },
+    error: function(error) {
+      throw "Got an error " + error.code + " : " + error.message;
+    }
+  });
+});
+
+// New Friend Push Notification
+Parse.Cloud.afterSave("Friendship", function(request) {
+
+  // Build fromUser
+  var fromUser = request.object.get('fromUserUsername');
+
+  // Build toUser
+  var toUser = request.object.get('toUser');
+
+  // Build the query
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo('userID', toUser.id);
+
+  Parse.Push.send({
+    where: pushQuery, // Set our Installation query
+    data: {
+      alert: fromUser+' has added you!',
+      sound: "default"
+    }
+  }, {
+    success: function() {
+      console.log('New Friend Push Notification Success');
     },
     error: function(error) {
       throw "Got an error " + error.code + " : " + error.message;
